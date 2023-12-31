9728d923fbcfd7d856fe6934601bbbced84c66b8
gy6543721 <gy6543721@yahoo.com.tw>
2023-12-31 17:13:03 +0900

    [Initial Update] project demo
    - Complete UI with State and ViewModel
    - Integrate Gemini api

---
+++ app/src/main/java/levi/lin/gemini/android/MainActivity.kt
@@ -0,0 +1,33 @@
+package levi.lin.gemini.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.google.ai.client.generativeai.GenerativeModel
+import levi.lin.gemini.android.ui.screen.GeminiRoute
+import levi.lin.gemini.android.ui.theme.GeminiAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GeminiAndroidTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    val generativeModel = GenerativeModel(
+                        modelName = "gemini-pro",
+                        apiKey = BuildConfig.apiKey
+                    )
+                    val viewModel = GeminiViewModel(generativeModel)
+                    GeminiRoute(viewModel)
+                }
+            }
+        }
+    }
+}
