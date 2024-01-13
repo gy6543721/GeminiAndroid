2b4f1895320cbe16e99af6355e9d9b5d2633a4ac
gy6543721 <gy6543721@yahoo.com.tw>
2024-01-13 17:53:18 +0900

    [Release] 0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "levi.lin.gemini.android"
         minSdk = 29
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
